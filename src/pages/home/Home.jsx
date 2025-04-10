8548f616d0c6d93272877329e14c8f424f4b7ab1
venky916 <venkateshsmsv1999@gmail.com>
2025-04-10 20:47:41 +0530

    routes issue

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/portfolio"); // make sure this route exists in your router config
+  };
+
   return (
     <div>
-      home
+      <h1>Home</h1>
+      <button onClick={handleClick}>Click me</button>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
